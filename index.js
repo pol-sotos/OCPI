0542b90c872822174143c16edee68c97facc1f4b
Pol Sotos <pol.sotos@webfleet.com>
2024-04-08 10:06:03 +0900

    Add 2.2 endpoints

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/ocpi/versions', (req, res) => {
             {
                 version: "2.1.1",
                 url: "https://ocpi.onrender.com/versions/2.1.1"
+            },
+            {
+                version: "2.2",
+                url: "https://ocpi.onrender.com/versions/2.2"
             }
         ],
         status_code: 1000,
@@ -55,11 +59,55 @@ app.get('/ocpi/2.1.1', (req, res) => {
         },
         status_code: 1000,
         status_message: "Success",
-        timestamp: new Date().toISOString() // Dynamically set to the current time
+        timestamp: new Date().toISOString() 
     };
 
     res.json(response);
 });
+
+app.get('/ocpi/2.2', (req, res) => {
+    const response = {
+        data: {
+            version: "2.2",
+            endpoints: [
+                {
+                    identifier: "credentials",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/credentials"
+                },
+                {
+                    identifier: "tokens",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/tokens"
+                },
+                {
+                    identifier: "commands",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/commands"
+                },
+                {
+                    identifier: "locations",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/locations"
+                },
+                {
+                    identifier: "sessions",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/sessions"
+                },
+                {
+                    identifier: "cdrs",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/cdrs"
+                },
+                {
+                    identifier: "tariffs",
+                    url: "https://ocpi.onrender.com/ocpi/2.2/tariffs"
+                }
+            ]
+        },
+        status_code: 1000,
+        status_message: "Success",
+        timestamp: new Date().toISOString()
+    };
+
+    res.json(response);
+});
+
 
 
 app.listen(port, () => {
