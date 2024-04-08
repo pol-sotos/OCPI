2e68a8dbc646cb43ace46e4c7aeb64247b16382d
Pol Sotos <pol.sotos@webfleet.com>
2024-04-08 09:08:48 +0900

    Use express

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get("/", (req, res) => res.type('html').send(html));
 
-const port = 3000;
-const hostname = '127.0.0.1';
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
